450b7f6dc9851e4b90c4a7f1e6aea99347b28db2
Bas Krahmer <baskrahmer@gmail.com>
2023-04-08 10:18:42 +0200

    Update lambda to take param

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,29 @@
-import boto3;
-
-boto3
+import json
+import random
 
 
 def lambda_handler(event, context):
-    result = "Hello World"
-    return {
-        'statusCode': 200,
-        'body': result
-    }
+    input = event.get("queryStringParameters", {}).get("product", None)
+    if input is None:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({"message": 'Please provide named parameter "product"'})
+        }
+    if random.random() < 0.5:
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                "pred": "Pastis (anise-flavoured spirit)",
+                "ef_score": 0.12303632999999999,
+                "confidence": 0.4,
+            })
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                "pred": "Cheese and ham, breaded",
+                "ef_score": 0.52999348,
+                "confidence": 0.6,
+            })
+        }
