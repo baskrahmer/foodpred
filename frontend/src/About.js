11ebdf32519a92d0fe978c330d7b3e0263c14b23
Bas Krahmer <baskrahmer@gmail.com>
2023-05-01 15:10:52 +0200

    update about

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -4,11 +4,12 @@ function About() {
   return (
     <main>
       <div className="container">
-        <h1>About EcoScore Calculator</h1>
+        <h1>About Foodpred</h1>
         <p>
-          EcoScore Calculator is a tool that allows users to estimate the ecological footprint of a food product. By
-          entering the name of a food product, the calculator will return the EcoScore and a probability value,
-          helping users make more informed choices about their consumption habits.
+          Foodpred is a tool that allows users to estimate the ecological footprint of a food product. By
+          entering the name of a food product, the tool uses a language model to predict the correct category.
+          This ideally helps users make fast and automated predictions about non-trivial products that are
+          not present in common databases like OpenFoodFacts.
         </p>
       </div>
     </main>
