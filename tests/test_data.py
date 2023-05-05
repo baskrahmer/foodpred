584632f4c272a6a67698275210229034ee4aba8f
Bas Krahmer <baskrahmer@gmail.com>
2023-05-06 01:20:58 +0200

    update test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,7 @@
+import os
+
 from harrygobert.data import get_dataloaders
+from harrygobert.model import get_tokenizer, get_tokenize_fn
 from harrygobert.util import parse_args
 
 
@@ -10,4 +13,7 @@ def test_data():
     # Smoketest data pipeline
     cfg = parse_args()
     cfg.debug = True
-    get_dataloaders(cfg)
+    cfg.csv_path = os.path.join(os.path.dirname(__file__), "test.csv")
+    tokenizer = get_tokenizer(cfg)
+    tokenize_fn = get_tokenize_fn(cfg, tokenizer)
+    get_dataloaders(cfg, tokenize_fn)
